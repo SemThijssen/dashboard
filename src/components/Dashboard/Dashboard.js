20018603c6fa3a6a519b2f3ee1b511a40979b7e5
Sem Thijssen <30442@ma-web.nl>
2022-11-09 12:14:02 +0100

    refactored dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,12 +2,11 @@ import React from "react"
 import LeftPane from "../LeftPane/LeftPane";
 import RightPane from "../RightPane/RightPane";
 import "./Dashboard.css"
-import disneycastleImage from "../../img/pexels-benjamin-suter-2362002.jpg";
-import pinnochioImage from "../../img/pinnochio.jpeg";
+import chooseImage from "../../helpers/chooseImage";
 import Popup from "../Popup/Popup";
-import WaltandMickeyImage from"../../img/pexels-david-guerrero-8183995.jpg";
-import WaltStarImage from"../../img/pexels-ekaterina-belinskaya-4700140.jpg";
+import productsObject from "../../data/products";
 
+import navigationItemsObject from "../../data/navigationItmes";
 
 class Dashboard extends React.Component  {
 
@@ -19,54 +18,22 @@ class Dashboard extends React.Component  {
     
 
     componentDidMount(){
-        let productCards = [
-            {
-                id: 1,
-                name:"Placeholder"
-        
-            },
-            {
-                id: 2,
-                name:"Disney Castle",
-                img: disneycastleImage
-            },
-            {
-                id: 3,
-                name:" Pinnochio" , 
-               img: pinnochioImage
-            },
-            {
-                id: 4,
-                name:" Pinnochio" , 
-               img: pinnochioImage
-            } 
-        ];
-        this.setState({productCards: productCards});
+       
+        this.setState({productCards: productsObject.products});
     }
     
     onButtonClicked = () => {
         this.setState({open: !this.state.open})
     }
     addButtonClicked = (inputFromPopup) =>{
+        let imageFromHelper =  chooseImage(inputFromPopup);
         let toBeAddedImage;
-      switch(inputFromPopup){
-        case("Pinnochio"):
-        toBeAddedImage = pinnochioImage;
-        break;
-        case("Disney Kasteel"):
-        toBeAddedImage = disneycastleImage;
-        break;
-        case("Walt en Mickey"):
-        toBeAddedImage = WaltandMickeyImage;
-        break;
-        default:
-        toBeAddedImage = WaltStarImage;
-        break;
-      }
+      
       let toBeAdded = [
         {
+            id:this.state.productCards.length +1,
             name: inputFromPopup,
-            img: toBeAddedImage
+            img: imageFromHelper
         }
     ]
     
@@ -77,33 +44,11 @@ class Dashboard extends React.Component  {
     })
     }
 render(){
-    let navigationListItems =
-    [
-        {
-            name: "Home",
-            message: 0,
-        },
-        {
-            name: "facturen",
-            message: 3,
-        },
-        {
-            name: "Bestellingen",
-            message: 0,
-        },
-        {
-            name: "Retour",
-            message: 1,
-        },
-        {
-            name: "Contact",
-            message: 2,
-        },
-        
-];
+ 
+    
 if (this.state.open === true){
     return( <article className="dashboard">
-    <LeftPane navigationListItems={navigationListItems} buttonText="Go Premium!"></LeftPane>
+    <LeftPane navigationListItems={navigationItemsObject.navigationItems} buttonText="Go Premium!"></LeftPane>
     <RightPane onButtonClicked={this.onButtonClicked}productCards={this.state.productCards}headerText={"Mijn Producten"} buttonSymbol="+" buttonText="Voeg een product toe"></RightPane>
 </article>
 );
